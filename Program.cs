f9f6b17ba6d525781b5a12b5dcf5bbf69a44b5f9
pruth <pruth@DESKTOP-MECPLOA>
2019-09-22 18:26:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace Assignment1_F19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 22, b = 33;
+            printSelfDividingNumbers(a, b);
+
+            int n2 = 5;
+            printSeries(n2);
+
+            int n3 = 5;
+            printTriangle(n3);
+
+            int[] J = new int[] { 1, 3 };
+            int[] S = new int[] { 1, 3, 3, 2, 2, 2, 2, 2 };
+            int r4 = numJewelsInStones(J, S);
+            Console.WriteLine(r4);
+
+            int[] arr1 = new int[] { 1, 2, 5, 6, 7, 8, 9 };
+            int[] arr2 = new int[] { 1, 2, 3, 4, 5 };
+            int[] r5 = getLargestCommonSubArray(arr1, arr2);
+            Console.WriteLine(r5);
+
+            solvePuzzle();
+            Console.ReadKey();
+
+        }
+
+        /*
+         * function: printSelfDividingNumbers()
+         * temp: Holds the number to be tested.
+         * digit: Individual digits of the number.
+         * flag: True if number is self dividing.
+         */
+        public static void printSelfDividingNumbers(int x, int y)
+        {
+            int temp, digit;       
+            bool flag;
+            try
+            {
+                for (int i = x; i <= y; i++)
+                {
+                    flag = true;
+                    temp = i;
+                    while (temp > 0)
+                    {
+                        digit = temp % 10;
+                        if (digit == 0 || i % digit != 0)
+                        {
+                            flag = false;
+                        }
+                        temp = temp / 10;
+                    }
+                    if (flag)                    
+                        Console.WriteLine("" + i + " is a self dividing no. ");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing printSelfDividingNumbers()");
+            }
+        }
+
+        public static void printSeries(int n)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing printSeries()");
+            }
+        }
+
+        public static void printTriangle(int n)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing printTriangle()");
+            }
+        }
+
+        public static int numJewelsInStones(int[] J, int[] S)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing numJewelsInStones()");
+            }
+
+            return 0;
+        }
+
+        public static int[] getLargestCommonSubArray(int[] a, int[] b)
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing getLargestCommonSubArray()");
+            }
+
+            return null; // return the actual array
+        }
+
+        public static void solvePuzzle()
+        {
+            try
+            {
+                // Write your code here
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing solvePuzzle()");
+            }
+        }
+    }
+}
